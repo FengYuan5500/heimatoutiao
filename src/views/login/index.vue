453c7b89243c65d282ba7138903b5d6067eb2540
‘ <fengyuanshishadiao@qq.com>
2020-01-09 21:01:20 +0800

    TOKen处理-存储

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,12 +4,12 @@
 
     <ValidationObserver ref='form'>
 
-   <ValidationProvider name='手机号' rules='required' v-slot="{errors}" immediate>
+   <ValidationProvider name='手机号' rules='required|mobile' v-slot="{errors}" immediate>
      <van-field v-model="user.mobile" clearable left-icon="contact" placeholder="请输入用户名">
      </van-field>
    </ValidationProvider>
 
-   <ValidationProvider name='验证码' rules='required' immediate>
+   <ValidationProvider name='验证码' rules='required|code' immediate>
         <van-field v-model="user.code" left-icon="passed" placeholder="请输入验证码">
           <van-count-down
             v-if="isCountDownShow"
@@ -39,6 +39,7 @@
 
 <script>
 import { login, getSmsCode } from '@/api/user'
+import { validate } from 'vee-validate'
 
 export default {
   name: 'LoginPage',
@@ -76,22 +77,37 @@ export default {
       })
 
       try {
-        const res = await login(user)
-        console.log(res)
+        const { data } = await login(user)
+        console.log(data)
+
+        // 将登录成功获取的用户token相关数据存储到Vuex容器
+        this.$store.commit('setUser', data.data)
+
         this.$toast.success('登录成功')
       } catch (err) {
         console.log('登录失败', err)
-        this.$toast.fail('登录失败')
+        this.$toast.fail('登录失败，手机号或验证码不正确')
       }
     },
     async onSendSmsCode () {
       try {
         const { mobile } = this.user
+
+        const validateResult = await validate(mobile, 'required|mobile', {
+          name: '手机号'
+        })
+        if (!validateResult.valid) {
+          console.log(validateResult)
+          this.$toast(validateResult.errors[0])
+          return
+        }
+        // 请求发送短信验证码
         const res = await getSmsCode(mobile)
         console.log(res)
+
         this.isCountDownShow = true
       } catch (err) {
-        console.log(err)
+        // 关闭验证码显示
         this.isCountDownShow = false
         this.$toast('不要频繁操作')
       }
